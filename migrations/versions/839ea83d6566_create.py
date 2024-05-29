0184e8606e938c0fab4eff8be50c3fa8bbb1e1ee
Amirali Qobadi <amiraliqobadi5@gmail.com>
2024-05-29 14:35:14 +0330

    migrations added

---
+++ migrations/versions/839ea83d6566_create.py
@@ -0,0 +1,26 @@
+"""create
+
+Revision ID: 839ea83d6566
+Revises: 
+Create Date: 2024-05-29 14:34:48.684638
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '839ea83d6566'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
